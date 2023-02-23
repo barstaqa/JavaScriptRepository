fa35df3ecb0bd3eff9851c3ec046b6819c8fb6a3
Bartosz Stachowski <b.stachowski@evionica.com>
2023-02-23 20:59:19 +0100

    add else if, prevent add only space

--- main.js
+++ main.js
@@ -14,10 +14,11 @@ app.get('/api/test/cases', (req, res) => {
 app.post('/api/test/cases', (req, res) => {
     const title = req.body.title;
     const description = req.body.description;
-    // TODO: Sprawdzić jak walidować tytuł ze spacją - aktualnie wysyła się post
     if (!title) {
         res.status(400).send('Test case should have a name, title is required.');
         return;
+    } else if (/^ +$/.test(title)) {
+        res.status(400).send('Test case should have a name, title cannot contain only spaces')
     };
 
     const testCase = testCasesRepository.addTestCase(title, description);
