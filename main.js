85b871af4e3b59567aae3667b526e106618085c7
Bartosz Stachowski <b.stachowski@evionica.com>
2023-03-01 16:03:30 +0100

    draft extract endpoint and improve whitespace

--- main.js
+++ main.js
@@ -1,29 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 9323;
-const { TestCasesRepository } = require('./TestCasesRepository');
 
 app.use(express.json());
+app.use('/api/test/cases', testCasesEndpoint);
 
-const testCasesRepository = new TestCasesRepository();
-
-app.get('/api/test/cases', (req, res) => {
-    res.send(testCasesRepository.getTestCases());
-});
-
-app.post('/api/test/cases', (req, res) => {
-    const title = req.body.title;
-    const description = req.body.description;
-    if (!title) {
-        res.status(400).send('Test case should have a name, title is required.');
-        return;
-    } else if (/^ +$/.test(title)) {
-        res.status(400).send('Test case should have a name, title cannot contain only spaces')
-    };
-
-    const testCase = testCasesRepository.addTestCase(title, description);
-    res.send(testCase);
-});
 
 app.listen(
     PORT,
