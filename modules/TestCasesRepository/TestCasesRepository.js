df765024f970ddc713e32a0dee3eda5d0d7518c3
Bartosz Stachowski <b.stachowski@evionica.com>
2023-03-08 16:35:53 +0100

    create request post suites in progress

--- modules/TestCasesRepository/TestCasesRepository.js
+++ modules/TestCasesRepository/TestCasesRepository.js
@@ -1,6 +1,7 @@
 class TestCasesRepository {
     constructor() {
         this.testCases = [];
+        this.suites = [];
         this.currentId = 1;
     }
 
@@ -8,15 +9,53 @@ class TestCasesRepository {
         return this.testCases;
     }
 
-    addTestCase(title, description) {
+    getTestSuites() {
+        return this.suites;
+    }
+
+    getTestCasesBySuite() {
+        const testCasesBySuite = {};
+        this.testCases.forEach((testCase) => {
+            const suiteId = testCase.suiteId;
+            const suiteTitle = suiteId ? this.suites.find((suite) => suite.id === suiteId).title : 'Undefined';
+            if (!testCasesBySuite[suiteTitle]) {
+                testCasesBySuite[suiteTitle] = [];
+            }
+            testCasesBySuite[suiteTitle].push(testCase);
+        });
+        return testCasesBySuite;
+    }
+
+    addTestCase(title, description, suiteId) {
         const newTestCase = {
             id: this.currentId++,
             title: title,
             description: description,
+            suiteId: suiteId,
         };
         this.testCases.push(newTestCase);
         return newTestCase;
     }
+    addSuite(title) {
+        const newTestSuite = {
+            suiteId: this.currentId++,
+            title: title,
+        }
+        this.suites.push(newTestSuite);
+        return newTestSuite;
+    }
+
+    editTestCase(id, title, description, suiteId) {
+        const testCase = this.testCases.find((testCase) => testCase.id === parseInt(id));
+        if (!testCase) {
+            return null;
+        }
+        testCase.title = title;
+        testCase.description = description;
+        testCase.suiteId = suiteId;
+        return testCase;
+    }
+
 }
 
 module.exports = { TestCasesRepository };
--- modules/TestCasesRepository/TestCasesRouter.js
+++ modules/TestCasesRepository/TestCasesRouter.js
@@ -7,18 +7,35 @@ const testCasesRepository = new TestCasesRepository();
 
 // Define routes for the TestCaseRouter
 TestCaseRouter.get('/', (req, res) => {
-    res.send(testCasesRepository.getTestCases());
+    res.send(testCasesRepository.getTestCasesBySuite());
 });
-
+TestCaseRouter.get('/', (req, res) => {
+    res.send(testCasesRepository.getTestSuites());
+});
 TestCaseRouter.post('/', (req, res) => {
     const title = req.body.title;
     const description = req.body.description;
+    const suiteId = req.body.suiteId;
     if (!isTestCaseValid(title)) {
         res.status(400).send('Test case should have a name, title is required.');
         return;
     };
 
-    const testCase = testCasesRepository.addTestCase(title, description);
+    const testCase = testCasesRepository.addTestCase(title, description, suiteId);
+    res.send(testCase);
+});
+TestCaseRouter.post('/', (req, res) => {
+    const title = req.body.title;
+    const suite = testCasesRepository.addSuite(title);
+    res.send(suite);
+});
+
+TestCaseRouter.put('/', (req, res) => {
+    const id = req.params;
+    const title = req.body.title;
+    const description = req.body.description;
+    const suiteId = req.body.suiteId;
+    const testCase = testCasesRepository.editTestCase(id, title, description, suiteId);
     res.send(testCase);
 });
 
