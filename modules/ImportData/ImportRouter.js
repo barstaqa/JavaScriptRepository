e1f49cf2d1201c5352a825514163450d22b8ac1a
Bartosz Stachowski <b.stachowski@evionica.com>
2023-03-22 19:58:34 +0100

    import data from qase

--- modules/ImportData/ImportRouter.js
+++ modules/ImportData/ImportRouter.js
@@ -3,16 +3,18 @@ const ImportRouter = express.Router();
 const { TestCasesRepository } = require('../TestCasesRepository/TestCasesRepository');
 const { SuitesRepository } = require('../SuitesRepository/SuitesRepository');
 ImportRouter.post('/', (req, res) => {
-    console.log(req.body);
-    const { title, testCases } = req.body;
+    const { suites } = req.body;
     const testCasesRepository = TestCasesRepository.getInstance();
     const suitesRepository = SuitesRepository.getInstance();
 
-    const suite = suitesRepository.addSuite(title);
-
-    testCases.forEach(testCase => {
-        testCasesRepository.addTestCase(testCase.title, testCase.description, suite.suiteId);
-    });
+    for (const suite of suites) {
+        const { title, cases } = suite;
+        const newSuite = suitesRepository.addSuite(title);
+        for (const testCase of cases) {
+            const { title, description } = testCase;
+            testCasesRepository.addTestCase(title, description, newSuite.suiteId);
+        }
+    }
 
     res.send({ message: "The data has been imported" });
 });
